6dd6e7f3b3fcd9d517c6035d00a78d18912c4d1a
ZairBalam <fzairb2@gmail.com>
2025-05-27 12:00:21 -0600

    primer boton

--- index.php
+++ index.php
@@ -3,16 +3,35 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>CRUD CON PHP, PDO, AJAX Y DATA TABLES.JS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/2.3.1/css/dataTables.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+    <div class="container">
+
+      <h1 class="text-center">CRUD CON PHP, PDO, AJAX Y DATA TABLES.JS</h1>
+
+      <h1 class="text-center">www.render2web.com</h1>
+      <div class="row">
+        <div class="col-2 offset-10">
+          <div class="text-center">
+            <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#modalUsuario" id="botonCrear">
+                  <i class="bi bi-plus-circle fill"></i> Crear Usuario
+                </button>
+
+          </div>
+        </div>
+
+    </div>
+    <br />
+    <br />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/2.3.1/js/dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
   </body>
-</html>
+</html>  
