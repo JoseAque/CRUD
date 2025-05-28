1ba5abdf7b926488b13ebfd5b399741514fe8b67
ZairBalam <fzairb2@gmail.com>
2025-05-27 20:21:58 -0600

    Estilos

--- index.php
+++ index.php
@@ -7,47 +7,47 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/2.3.1/css/dataTables.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styles.css">
   </head>
   <body>
 
-    <div class="container">
+    <div class="container fondo">
 
-      <h1 class="text-center">CRUD CON PHP, PDO, AJAX Y DATA TABLES.JS</h1>
+        <h1 class="text-center">CRUD CON PHP, PDO, AJAX Y DATA TABLES.JS</h1>
 
-      <!--<h1 class="text-center">www.render2web.com</h1> -->
-      <div class="row">
-        <div class="col-2 offset-10">
-          <div class="text-center">
-            <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#modalUsuario" id="botonCrear">
-                  <i class="bi bi-plus-circle fill"></i> Crear 
-                </button>
+        <!--<h1 class="text-center">www.render2web.com</h1> -->
+        <div class="row">
+          <div class="col-2 offset-10">
+            <div class="text-center">
+              <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#modalUsuario" id="botonCrear">
+                    <i class="bi bi-plus-circle fill"></i> Crear 
+                  </button>
 
+            </div>
           </div>
-        </div>
 
-    </div>
-    <br />
-    <br />
+      </div>
+      <br />
+      <br />
 
-    <div class="table-responsive">
-      <table id="datos_usuario" class="table table-bordered table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>Teléfono</th>
-            <th>Email</th>
-            <th>Imagen</th>
-            <th>Fecha de Creación</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-          </tr>
-        </thead>
-      </table>
-    </div>
+      <div class="table-responsive">
+        <table id="datos_usuario" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Apellidos</th>
+              <th>Teléfono</th>
+              <th>Email</th>
+              <th>Imagen</th>
+              <th>Fecha de Creación</th>
+              <th>Editar</th>
+              <th>Eliminar</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
   </div>
 
   <!-- Modal -->
